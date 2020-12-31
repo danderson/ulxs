b762a3b91e0a5e633c94a69434a7ffa24d2856fe
David Anderson <dave@natulte.net>
2020-12-31 02:34:16 -0800

    Initialize.

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = { # for shell.nix
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            goimports
+            python3
+            gtkwave
+            git
+            yosys
+            symbiyosys
+            trellis
+            nextpnr
+            verilator
+            fujprog
+            dfu-util
+          ];
+        };
+      });
+}
