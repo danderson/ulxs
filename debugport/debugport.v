ddfb333e56f3369f69d60287e64a00f823cc8506
David Anderson <dave@natulte.net>
2021-02-07 22:14:37 -0800

    Up debugport speed to 115200 baud.

--- debugport/debugport.v
+++ debugport/debugport.v
@@ -5,8 +5,8 @@ module debugport(input clk_25mhz,
 				 output wifi_gpio0);
    wire [7:0] uart_byte;
    wire uart_ready;
-   // input 25000000Hz, output 9600Hz, 16x oversample, error 0.33%
-   uart #(.baud_acc_width(14), .baud_acc_incr(101))
+   // input 25000000Hz, output 115200Hz, 16x oversample, error 0.0033%
+   uart #(.baud_acc_width(11), .baud_acc_incr(151))
      uart(.i_clk(clk_25mhz), .i_rx(ftdi_txd), .o_byte(uart_byte), .o_ready(uart_ready));
 
    // Only display a byte when it's been completely read-in.
